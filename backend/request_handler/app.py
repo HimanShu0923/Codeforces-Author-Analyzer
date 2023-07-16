feaa20142d80ed453e6f87d17d7a0278378014ed
HimanShu0923 <20bcs102@iiitdmj.ac.in>
2023-07-16 18:32:57 +0530

    completed request_handler lambda

--- backend/request_handler/app.py
+++ backend/request_handler/app.py
@@ -1,22 +1,57 @@
 import json
-# import requests
+import boto3
+dynamodb = boto3.resource("dynamodb")
+TABLE_NAME = "Codeforces-Author-Analyzer"
+CODEFORCES_BASE_URL = "https://codeforces.com/problemset/problem"
+table = dynamodb.Table(TABLE_NAME)
+
+def create_url(contest_id, index):
+    return f"{CODEFORCES_BASE_URL}/{contest_id}/{index}"
 
 
 def lambda_handler(event, context):
     """
     Request handler : takes author name as input and provide problem info as output
 
-    input : {"body" : <string>}
-    outptut : { output schema }
+    input : {"body" : {"author" : <string>}}
+    
+    outptut : {"tag name" : []problem_obj}
+    problem_obj : {"problem_url" : string, "name" : string}
     """
     body = json.loads(event.get("body"))
     author=  body.get("author")
-
-    #TODO: Fetch data from database and return the data.
-
+    
+    response = table.get_item(
+        TableName=TABLE_NAME,
+        Key={
+            'AuthorID': author
+        }
+    )
+    
+    row = response.get('Item')
+    if row is None:
+        return {
+            "statusCode" : 404,
+            "body" : json.dumps({
+                "message" : f"No contest made by : {author}"
+            })
+        }
+        
+    problems = row.get("Problems")
+    response = {}
+    
+    for problem in problems:
+        problem_obj = {"problem_url" : create_url(problem.get("contestId"), problem.get("index")), "name" : problem.get("name")}
+        tags = problem.get("tags")
+        for tag in tags:
+            if tag in response:
+                response[tag].append(problem_obj)
+            else:
+                response[tag] = [problem_obj]
+    
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": f"Author name : {author}"
+            "message": f"{response}"
         }),
     }
